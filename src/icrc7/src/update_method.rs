10f23fcc73b16155d83dd51c85a0e12cd784df40
av1ctor <av1ctortv@gmail.com>
2025-03-26 13:16:23 -0300

    feat: added the mint_with_approval method

--- src/icrc7/src/update_method.rs
+++ src/icrc7/src/update_method.rs
@@ -2,8 +2,8 @@ use candid::Principal;
 use ic_cdk_macros::update;
 
 use crate::{
-    guards::owner_guard, state::STATE, BurnArg, BurnResult, MintArg, MintResult, TransferArg,
-    TransferResult,
+    guards::owner_guard, icrc37_types::{ApprovalInfo, ApproveTokenArg}, state::STATE, utils::principal_to_subaccount, 
+    BurnArg, BurnResult, MintArg, MintResult, TransferArg, TransferResult
 };
 use icrc_ledger_types::icrc1::account::Account;
 
@@ -23,6 +23,34 @@ pub fn mint(arg: MintArg) -> MintResult {
         });
     }
     STATE.with(|s| s.borrow_mut().mint(&caller, arg))
+}
+
+#[update]
+pub fn mint_with_approval(arg: MintArg) -> MintResult {
+    let minting_authority = Account { 
+        owner: STATE.with_borrow(|s| s.minting_authority.unwrap().owner), 
+        // use the owner's principal as subaccount
+        subaccount: Some(principal_to_subaccount(&arg.to.owner))
+    };
+    
+    // 1st: try to mint (only the minting authority is allowed)
+    let token_id = mint(arg)?;
+
+    // 2nd: approve transfers done by the minting authority
+    let args = vec![ApproveTokenArg { 
+        token_id, 
+        approval_info: ApprovalInfo::new(
+            None, 
+            minting_authority, 
+            None, 
+            None, 
+            Some(ic_cdk::api::time())
+        )
+    }];
+
+    STATE.with(|s| s.borrow_mut().approve(&ic_cdk::caller(), args));
+
+    Ok(token_id)
 }
 
 #[update]
