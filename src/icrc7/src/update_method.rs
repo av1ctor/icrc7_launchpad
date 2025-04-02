cce2f3e62a1ecf992e8ea46325cef061e1aa10ff
av1ctor <av1ctortv@gmail.com>
2025-04-02 12:25:02 -0300

    chore: removed mint() and set_minting_authority()

--- src/icrc7/src/update_method.rs
+++ src/icrc7/src/update_method.rs
@@ -1,27 +1,15 @@
 use candid::Principal;
 use ic_cdk_macros::update;
 use crate::{
-    guards::owner_guard, icrc37_types::{ApprovalInfo, ApproveTokenArg}, state::STATE, 
+    icrc37_types::{ApprovalInfo, ApproveTokenArg}, 
+    state::STATE, 
     BurnArg, BurnResult, MintArg, MintResult, TransferArg, TransferResult
 };
-use icrc_ledger_types::icrc1::account::Account;
 
 #[update]
 pub fn icrc7_transfer(args: Vec<TransferArg>) -> Vec<Option<TransferResult>> {
     let caller = ic_cdk::caller();
     STATE.with(|s| s.borrow_mut().icrc7_transfer(&caller, args))
-}
-
-#[update]
-pub fn mint(arg: MintArg) -> MintResult {
-    let caller = ic_cdk::caller();
-    if caller == Principal::anonymous() {
-        return Err(crate::errors::MintError::GenericBatchError {
-            error_code: 100,
-            message: "Anonymous Identity".into(),
-        });
-    }
-    STATE.with(|s| s.borrow_mut().mint(&caller, arg))
 }
 
 #[update]
@@ -33,21 +21,25 @@ pub fn mint_with_approval(arg: MintArg) -> MintResult {
     let owner = arg.to.owner.clone();
     
     // 1st: try to mint (only the minting authority is allowed)
-    let token_id = mint(arg)?;
+    let token_id = STATE.with_borrow_mut(|s| { 
+        s.mint(&ic_cdk::caller(), arg)
+    })?;
 
-    // 2nd: approve transfers done by the minting authority
-    let args = vec![ApproveTokenArg { 
-        token_id, 
-        approval_info: ApprovalInfo::new(
-            None, 
-            minting_authority, 
-            None, 
-            None, 
-            Some(ic_cdk::api::time())
-        )
-    }];
+    // 2nd: approve transfers to be done by the minting authority
+    STATE.with_borrow_mut(|s| {
+        let args = vec![ApproveTokenArg { 
+            token_id, 
+            approval_info: ApprovalInfo::new(
+                None, 
+                minting_authority, 
+                None, 
+                None, 
+                Some(ic_cdk::api::time())
+            )
+        }];
 
-    STATE.with(|s| s.borrow_mut().approve(&owner, args));
+        s.approve(&owner, args)
+    });
 
     Ok(token_id)
 }
@@ -56,10 +48,4 @@ pub fn mint_with_approval(arg: MintArg) -> MintResult {
 pub fn burn(args: Vec<BurnArg>) -> Vec<Option<BurnResult>> {
     let caller = ic_cdk::caller();
     STATE.with(|s| s.borrow_mut().burn(&caller, args))
-}
-
-#[update(guard = "owner_guard")]
-pub fn set_minting_authority(minting_account: Account) -> bool {
-    STATE.with(|s| s.borrow_mut().minting_authority = Some(minting_account));
-    return true;
 }
